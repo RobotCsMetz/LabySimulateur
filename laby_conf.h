90f862de499db758a5fdb348b2630ccdde4213fb
DrBlackApple <johan.herlent@free.fr>
2022-10-04 16:43:03 +0200

    Réparation à cause de Aurélian

---
+++ laby_conf.h
@@ -0,0 +1,10 @@
+#ifndef LABY_CONF_HEADER
+#define LABY_CONF_HEADER
+
+#define LABY_CELL_NUMBER 11
+#define LABY_CELL_SIZE 180
+#define WALL_WIDTH 12
+
+#define DRAW_SCALE 10
+
+#endif
--- main.c
+++ main.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "laby_conf.h"
 #include "gui/gui.h"
 
 int main(int argc, char** argv)
 {
-    laby_gen();
-    init_gui();
+    SDL_Event event;
+    uint8_t matrix[LABY_CELL_NUMBER][LABY_CELL_NUMBER];
+    memset((void *)matrix, 0xffff, LABY_CELL_NUMBER*LABY_CELL_NUMBER/2);
+
+    init_gui((uint8_t **)matrix);
+
+    while(1) {
+        SDL_PollEvent(&event);
+        switch (event.type) {
+            case SDL_QUIT:
+                goto noerror;
+                break;
+
+            default:
+                break;
+        }
+
+        refresh_gui(0, 0, 0, 0);
+
+        SDL_Delay(10);
+    }
+
+    fprintf(stdout, "Erreur lors de la récupération d'évenement (%s)", SDL_GetError());
+
+noerror:
+    destroy_gui();
 
     return EXIT_SUCCESS;
-}
-
-void laby_gen()
-{
-
 }
